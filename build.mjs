ea7c465caf56e00bf073ba36e172b3467c102f6d
Matjah Sonneveld <matjah@matjah.eu>
2021-12-30 13:27:33 +0100

    chore: update gist url

--- build.mjs
+++ build.mjs
@@ -4,7 +4,7 @@ import got from "got";
 const THEME_NAME = "elegant";
 const OUTPUT_DIR = "dist";
 const RESUME_JSON_URL =
-  "https://gist.githubusercontent.com/matjahs/00071c5d8c74d4a9c7b88b856b31fd63/raw/79ce316c1ad55793b5913f5c9d556f6475ab3726/resume.json";
+  "https://gist.githubusercontent.com/matjahs/00071c5d8c74d4a9c7b88b856b31fd63/raw/8ec0e8f7d9e9b549c2b0cd6b436de62ae641ee0c/resume.json";
 
 // Import theme
 const theme = await import(`jsonresume-theme-${THEME_NAME}`);
