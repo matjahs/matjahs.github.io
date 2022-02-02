bd4a1c0e9adc256d65d99e5434c1b137d74077f6
Matjah Sonneveld <matjah@matjah.eu>
2022-02-02 20:23:59 +0100

    feat: add clearbitjs script

--- build.mjs
+++ build.mjs
@@ -20,7 +20,18 @@ const html = theme.render(resume);
 if (!fs.existsSync(OUTPUT_DIR)) {
   fs.mkdirSync(OUTPUT_DIR);
 }
+}
+const script = `<script type="text/javascript">
+  (function (d, u, h, s) {
+    h = d.getElementsByTagName('head')[0];
+    s = d.createElement('script');
+    s.async = 1;
+    s.src = u + new Date().getTime();
+    h.appendChild(s);
+  })(document, 'https://grow.clearbitjs.com/api/pixel.js?v=');
+</script>`
+const injectScript = (html) => html.replace("<body>", script + "<body>");
 // Write the HTML to the output directory
-fs.writeFileSync("./dist/index.html", html);
+fs.writeFileSync("./dist/index.html", injectScript(html));
 
 console.log(`Built resume with ${THEME_NAME} theme into ./dist/index.html`);
